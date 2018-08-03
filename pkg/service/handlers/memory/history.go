efef5bee6f7ca3a614b8d2c7eeea1d503e311f00
Ilia Donchenko <iliad@me.com>
2018-08-03 17:39:42 +0300

    Add websocket methods

--- pkg/service/handlers/memory/history.go
+++ pkg/service/handlers/memory/history.go
@@ -1,13 +1,17 @@
 package memory
 
 import (
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/containerum/cherry/adaptors/gonic"
 	"github.com/containerum/nodeMetrics/pkg/meterrs"
 	"github.com/containerum/nodeMetrics/pkg/metrics"
 	"github.com/containerum/nodeMetrics/pkg/service/handlers"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"github.com/json-iterator/go"
 	"github.com/sirupsen/logrus"
 )
 
@@ -50,5 +54,53 @@ func NodeHistory(metrics metrics.Metrics) func(ctx *gin.Context) {
 		}
 		logrus.Debugf("writing response")
 		ctx.JSON(http.StatusOK, memoryHistory)
+	}
+}
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func HistoryWS(metrics metrics.Metrics) func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+		logrus.Debugf("START GET metrics MEMORY")
+		defer logrus.Debugf("END GET metrics MEMORY")
+
+		fromToStep := handlers.FromToStep{}
+		fromToStep.From = time.Now().Add(-1 * time.Hour)
+		fromToStep.To = time.Now()
+		fromToStep.Step = 1 * time.Minute
+
+		c, err := upgrader.Upgrade(ctx.Writer, ctx.Request, nil)
+		if err != nil {
+			log.Print("upgrade:", err)
+			return
+		}
+		defer c.Close()
+		for {
+			logrus.Debugf("%+v %d points", fromToStep, fromToStep.To.Sub(fromToStep.From)/fromToStep.Step)
+			memoryHistory, err := metrics.MemoryNodesHistory(fromToStep.From, fromToStep.To, fromToStep.Step)
+			if err != nil {
+				gonic.Gonic(meterrs.ErrUnableToGetMemoryHistory().AddDetailsErr(err), ctx)
+				return
+			}
+			logrus.Debugf("writing response")
+
+			text, _ := jsoniter.Marshal(memoryHistory)
+			if err != nil {
+				gonic.Gonic(meterrs.ErrUnableToGetMemoryHistory().AddDetailsErr(err), ctx)
+				return
+			}
+
+			err = c.WriteMessage(1, text)
+			if err != nil {
+				log.Println("write:", err)
+				break
+			}
+			time.Sleep(1 * time.Minute)
+
+			fromToStep.From = time.Now()
+			fromToStep.To = time.Now()
+			fromToStep.Step = 1 * time.Minute
+		}
+
 	}
 }
